d0867aeb6b6be07172dcab76e23191cdbc25adb9
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-04-30 20:18:16 -0300

    shorten the function

--- process.py
+++ process.py
@@ -1,12 +1,9 @@
 import json
 import pandas as pd
 
-def json_to_csv(json, csv):
-  df = pd.DataFrame(json["mvrv"])
-  df.columns = ["date", "mvrv"]
-  df.to_csv(csv, index=False)
-
 if __name__ == "__main__":
   with open('data/mvrv.json', 'r') as file:
     json = json.load(file)
-    json_to_csv(json, 'data/dataset.csv')
+    df = pd.DataFrame(json["mvrv"])
+    df.columns = ["date", "mvrv"]
+    df.to_csv('data/dataset.csv', index=False)
