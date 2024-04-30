2f48ee6444b94c6ed93d4b4894ef90370e06571e
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-04-30 20:13:22 -0300

    generate dataset.csv

--- process.py
+++ process.py
@@ -1,6 +1,12 @@
-import datetime
+import json
+import pandas as pd
 
-timestamp = 1230940800000 / 1000  # Seconds since Unix epoch
-date = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d')
+def json_to_csv(json, csv):
+  df = pd.DataFrame(json["mvrv"])
+  df.columns = ["date", "mvrv"]
+  df.to_csv(csv, index=False)
 
-print(date)
+if __name__ == "__main__":
+  with open('data/mvrv.json', 'r') as file:
+    json = json.load(file)
+    json_to_csv(json, 'data/dataset.csv')
