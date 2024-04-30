a0b08d32e9c3a8db31a90cec7b506af0fda39952
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-04-30 20:32:41 -0300

    convert date

--- process.py
+++ process.py
@@ -1,9 +1,13 @@
 import json
 import pandas as pd
 
+
 if __name__ == "__main__":
   with open('data/mvrv.json', 'r') as file:
     json = json.load(file)
     df = pd.DataFrame(json["mvrv"])
     df.columns = ["date", "mvrv"]
+    df["date"] = df["date"].apply(lambda x: pd.to_datetime(x, unit='ms').strftime('%Y-%m-%d'))
+
+
     df.to_csv('data/dataset.csv', index=False)
