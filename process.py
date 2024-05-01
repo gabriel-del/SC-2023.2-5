83927142eff39cb968352d5ca6a408178bcce04a
Gabriel Del <gabrieldelc.barros@gmail.com>
2024-05-01 04:25:27 -0300

    generate function to join parameters

--- process.py
+++ process.py
@@ -1,26 +1,21 @@
 import json
 import pandas as pd
 
-def a():
-  with open('data/mvrv.json', 'r') as file:
+def f(name, index=None):
+  if index is None: index = name
+  with open(f'data/{name}.json', 'r') as file:
     json_file = json.load(file)
-    df_mvrv = pd.DataFrame(json_file["mvrv"])
-    df_market_price = pd.DataFrame(json_file["market-price"])
-    df_mvrv.columns = ["date", "mvrv"]
-    df_market_price.columns = ["date", "market-price"]
-    return pd.merge( df_market_price,df_mvrv, on="date", how="outer")
-
-def b():
-  with open('data/200w.json', 'r') as file:
-    json_file = json.load(file)
-    df_2 = pd.DataFrame(json_file["200w-moving-avg-heatmap"])
     df_1 = pd.DataFrame(json_file["market-price"])
-    df_2.columns = ["date", "200w"]
+    df_2 = pd.DataFrame(json_file[index])
     df_1.columns = ["date", "market-price"]
+    df_2.columns = ["date", name]
     return pd.merge( df_1,df_2, on="date", how="outer")
 
 
-df = pd.merge( a(),b(), how="outer")
+df = pd.merge(
+  f('mvrv'),
+  f('200w', '200w-moving-avg-heatmap'),
+  how="outer")
 df = df.sort_values(by="date")
 df["date"] = pd.to_datetime(df["date"], unit='ms').dt.strftime('%Y%m%d%H%M%S')
 print(df)
